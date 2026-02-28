fc69ec947ee28e9e72449202e263ccaa57979131
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2026-02-28 13:03:35 +0100

    java 25

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Standard Java implementation af Main uden preview-features.
+ */
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    // Standard entry point kræver String[] args og public access
+    public static void main(String[] args) {
+
+        // System.out er en PrintStream instans. Det er standarden for konsol-output.
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+            // Vi kalder metoden på PrintStream objektet direkte
+            System.out.println("i = " + i);
         }
     }
 }
