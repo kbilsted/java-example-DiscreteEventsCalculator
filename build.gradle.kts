34153b43eab5cd2fd94ac762e8207132bf11a896
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2026-02-28 18:33:27 +0100

    java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,14 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
