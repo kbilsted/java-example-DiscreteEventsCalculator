85cff4bd4d0609270978943ff7996c7a896449d5
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2026-03-01 19:40:26 +0100

    test

--- src/test/java/org/bffs/DocumentStoreTest.java
+++ src/test/java/org/bffs/DocumentStoreTest.java
@@ -2,11 +2,14 @@ package org.bffs;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.models.Event;
 import org.models.Person;
 import org.storage.DocumentStore;
 import org.storage.GlobalId;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,7 +32,20 @@ class DocumentStoreTest {
         bffApi.createPaymentEvent(person, Instant.parse("2026-01-01T00:00:00Z"), 100);
 
         assertEquals(1, store.people().size());
-        assertEquals("payment", store.timelines().get(person.id()).events().getFirst().name());
-        assertEquals(100, store.timelines().get(person.id()).events().getFirst().generations().getFirst().input().inputs().get("amount"));
+        assertEquals(1, store.timelines().size());
+        assertEquals("payment", store.timelines().get(person.id()).events().getLast().name());
+        assertEquals(100, store.timelines().get(person.id()).events().getLast().generations().getLast().input().inputs().get("amount"));
+    }
+
+    @Test
+    void createsPersonAndPaymentsEventWithInput() {
+        bffApi.createPaymentEvent(person, Instant.parse("2026-01-01T00:00:00Z"), 100);
+        bffApi.createPaymentEvent(person, Instant.parse("2026-02-01T00:00:00Z"), 110);
+
+        List<Event> events = store.timelines().get(person.id()).events();
+        assertEquals(2, events.size());
+        HashMap<Integer, Integer> paymentsPerYear = events.getLast().generations().getLast().state().paymentsPerYear();
+        assertEquals(null, paymentsPerYear.get(2025));
+        assertEquals(210, paymentsPerYear.get(2026));
     }
 }
